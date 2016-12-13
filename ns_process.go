82045b31da7860b8cff50564b325e19757bc6a42
teddyking <ed@teddyking.co.uk>
2016-12-13 10:33:32 +0000

    Add {Uid,Gid}Mappings

--- ns_process.go
+++ ns_process.go
@@ -22,6 +22,20 @@ func main() {
 			syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNET |
 			syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
 	}
 
 	if err := cmd.Run(); err != nil {
--- ns_process_test.go
+++ ns_process_test.go
@@ -14,10 +14,11 @@ import (
 
 var _ = Describe("The ns-process CLI", func() {
 	var (
-		command *exec.Cmd
-		session *gexec.Session
-		stdin   io.WriteCloser
-		stdout  *gbytes.Buffer
+		command                   *exec.Cmd
+		session                   *gexec.Session
+		stdin                     io.WriteCloser
+		cmdToRunInNamespacedShell string
+		stdout                    *gbytes.Buffer
 	)
 
 	BeforeEach(func() {
@@ -27,15 +28,16 @@ var _ = Describe("The ns-process CLI", func() {
 		stdin, err = command.StdinPipe()
 		Expect(err).NotTo(HaveOccurred())
 		stdout = gbytes.NewBuffer()
-
-		stdinWriter := bufio.NewWriter(stdin)
-		stdinWriter.WriteString("ls -lah /proc/self/ns")
-		stdinWriter.Flush()
-		Expect(stdin.Close()).To(Succeed())
+		cmdToRunInNamespacedShell = "true"
 	})
 
 	JustBeforeEach(func() {
 		var err error
+
+		stdinWriter := bufio.NewWriter(stdin)
+		stdinWriter.WriteString(cmdToRunInNamespacedShell)
+		stdinWriter.Flush()
+		Expect(stdin.Close()).To(Succeed())
 
 		session, err = gexec.Start(command, stdout, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
@@ -49,12 +51,32 @@ var _ = Describe("The ns-process CLI", func() {
 		Eventually(session).Should(gexec.Exit(0))
 	})
 
-	It("starts a /bin/sh process in a new set of namespaces", func() {
-		namespaces := []string{"mnt", "uts", "ipc", "pid", "net", "user"}
+	Describe("cloning namespaces", func() {
+		BeforeEach(func() {
+			cmdToRunInNamespacedShell = "ls -lah /proc/self/ns"
+		})
 
-		for _, namespace := range namespaces {
-			currentInode := inode("self", namespace)
-			Consistently(stdout).ShouldNot(gbytes.Say(currentInode))
-		}
+		It("starts a /bin/sh process in a new set of namespaces", func() {
+			namespaces := []string{"mnt", "uts", "ipc", "pid", "net", "user"}
+
+			for _, namespace := range namespaces {
+				currentInode := inode("self", namespace)
+				Consistently(stdout).ShouldNot(gbytes.Say(currentInode))
+			}
+		})
+	})
+
+	Describe("user namespace configuration", func() {
+		BeforeEach(func() {
+			cmdToRunInNamespacedShell = "id"
+		})
+
+		It("applies a UID mapping", func() {
+			Eventually(stdout).Should(gbytes.Say(`uid=0\(root\)`))
+		})
+
+		It("applies a GID mapping", func() {
+			Eventually(stdout).Should(gbytes.Say(`gid=0\(root\)`))
+		})
 	})
 })
